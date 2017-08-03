2238666d2cb00aa87da94bd55c911cd7fec79552
Elder Moraes <elder.moraes@gmail.com>
2017-08-03 01:17:26 -0300

    Added chapter and template for the first recipe

---
+++ chapter01/beanvalidation/beanvalidation/src/main/java/com/eldermoraes/beanvalidation/BeanValidation.java
@@ -0,0 +1,17 @@
+package com.eldermoraes.beanvalidation;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.constraints.Positive;
+
+/**
+ *
+ * @author eldermoraes
+ */
+public class BeanValidation {
+
+    public static void main(String[] args){
+    }
+    
+    
+}
