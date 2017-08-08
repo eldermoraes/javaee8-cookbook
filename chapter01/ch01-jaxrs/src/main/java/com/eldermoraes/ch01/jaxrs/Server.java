aa8a6d3d87b030445a584fa7b12dc016608c0d0e
Elder Moraes <elder.moraes@gmail.com>
2017-08-08 02:35:42 -0300

    JAX-RS 2.1 recipe

--- chapter01/ch01-jaxrs/src/main/java/com/eldermoraes/ch01/jaxrs/Server.java
+++ chapter01/ch01-jaxrs/src/main/java/com/eldermoraes/ch01/jaxrs/Server.java
@@ -1,46 +1,90 @@
 package com.eldermoraes.ch01.jaxrs;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import javax.ejb.Stateless;
-import javax.enterprise.inject.Produces;
+import java.io.IOException;
+
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.sse.Sse;
 import javax.ws.rs.sse.SseEventSink;
 
-/**
- *
- * @author eldermoraes
- */
-@Stateless
-@Path("servers")
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedExecutorService;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Path("ss-events")
+@Singleton
 public class Server {
 
+    @Resource
+    private ManagedExecutorService executor;
+
+    private final Sse sse;
+    private SseEventSink sseSink;
+
+    @Inject
+    public Server(Sse sse) {
+        this.sse = sse;
+    }
+
     @GET
-    @Path("eventStream")
     @Produces(MediaType.SERVER_SENT_EVENTS)
-    public void eventStream(@Context SseEventSink sseSink, @Context Sse sse) {
-        
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        executor.execute(() -> {
-            try (SseEventSink sink = sseSink) {
-                sseSink.send(sse.newEvent("eventA"));
-                //sseSink.onNext(sse.newEvent("eventA"));
+    public void getMessageQueue(@Context SseEventSink eventSink) {
+        if (this.sseSink != null) {
+            throw new IllegalStateException("Sink already instantiated.");
+        }
+        this.sseSink = eventSink;
+    }
 
-                sseSink.send(sse.newEvent("eventB"));
-                //sseSink.onNext(sse.newEvent("eventB"));
-                
-                sseSink.send(sse.newEvent("eventC"));
-                
-                //sseSink.onNext(sse.newEvent("eventC"));
+    @POST
+    public void addMessage(final String message) throws IOException {
+        if (sseSink == null) {
+            throw new IllegalStateException("No sink available.");
+        }
+        sseSink.send(sse.newEvent("jax-rs 2.1"));
+    }
 
-                sseSink.close();
-                // handle exception
+    @DELETE
+    public void close() throws IOException {
+        if (sseSink != null) {
+            sseSink.close();
+            sseSink = null;
+        }
+    }
+
+    @POST
+    @Path("sendEvent/{id}")
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    public void sendEvent(@PathParam("id") String id,
+                          @Context SseEventSink sink) {
+
+        executor.submit(() -> {
+            try {
+                sink.send(sse.newEventBuilder().name("steps")
+                                  .data(String.class, "sending event " + id + " ...").build());
+                Thread.sleep(200);
+                
+                sink.send(sse.newEvent("step", "1"));
+                Thread.sleep(200);
+                
+                sink.send(sse.newEvent("step", "2"));
+                Thread.sleep(200);
+                
+                sink.send(sse.newEvent("step", "3"));
+                Thread.sleep(200);
+                
+                sink.send(sse.newEvent("step", "Final"));
+                sink.close();
+                
+            } catch (final InterruptedException e) {
+                System.out.println(e.getMessage());
             }
         });
     }
-
 }
