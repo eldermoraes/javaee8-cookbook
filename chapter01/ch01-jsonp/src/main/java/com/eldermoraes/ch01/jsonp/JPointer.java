92b3a543eed402b67a7279c4570d7004f0aae046
Elder Moraes <elder.moraes@gmail.com>
2017-08-09 07:11:13 -0300

    JsonP reference

---
+++ chapter01/ch01-jsonp/src/main/java/com/eldermoraes/ch01/jsonp/JPointer.java
@@ -0,0 +1,30 @@
+package com.eldermoraes.ch01.jsonp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import javax.json.Json;
+import javax.json.JsonPointer;
+import javax.json.JsonReader;
+import javax.json.JsonStructure;
+import javax.json.JsonValue;
+import org.glassfish.json.JsonPointerImpl;
+
+/**
+ *
+ * @author eldermoraes
+ */
+public class JPointer {
+    
+    
+    public static void main(String[] args) throws IOException{
+        try (InputStream is = JPointer.class.getResourceAsStream("/user.json");
+             JsonReader jr = Json.createReader(is)) {
+            
+            JsonStructure js = jr.read();
+            JsonPointer jp = new JsonPointerImpl("/user/name");
+            JsonValue jv = jp.getValue(js);
+            System.out.println("value: " + jv);
+
+        }
+    }
+}
