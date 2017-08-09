a85bfb0cc1c2cc273b9ede3eaa02652ce16fd4ca
Elder Moraes <elder@eldermoraes.com>
2017-08-09 17:34:01 -0300

    MVC recipe

--- chapter01/ch01-mvc/src/main/java/com/eldermoraes/ch01/mvc/AppConfig.java
+++ chapter01/ch01-mvc/src/main/java/com/eldermoraes/ch01/mvc/AppConfig.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.eldermoraes.ch01.mvc;
 
 import javax.ws.rs.ApplicationPath;
@@ -14,5 +9,5 @@ import javax.ws.rs.core.Application;
  */
 @ApplicationPath("view")
 public class AppConfig extends Application{
-    //http://localhost:8080/ch01-mvc/view/userController
+
 }
