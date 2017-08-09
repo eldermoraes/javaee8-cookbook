d567eaa8a611b2ff20596527fdc11f9273af6a95
Elder Moraes <elder.moraes@gmail.com>
2017-08-09 07:42:46 -0300

    Servlet 4.0 reference

---
+++ chapter01/ch01-servlet/src/main/java/com/eldermoraes/ch01/servlet/ServerPush.java
@@ -0,0 +1,41 @@
+package com.eldermoraes.ch01.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.PushBuilder;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author eldermoraes
+ */
+@WebServlet(name = "ServerPush", urlPatterns = {"/ServerPush"})
+public class ServerPush extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PushBuilder pb = request.newPushBuilder();
+        if (pb != null) {
+            pb.path("images/javaee-logo.png")
+              .addHeader("content-type", "image/png")
+              .push();
+        }
+
+        try (PrintWriter writer = response.getWriter();) {
+            StringBuilder html = new StringBuilder();
+            html.append("<html>");
+            html.append("<center>");
+            html.append("<img src='images/javaee-logo.png'><br>");
+            html.append("<h3>This image was pushed by ServerPush</h3>");
+            html.append("</center>");
+            html.append("</html>");
+            writer.write(html.toString());
+        }
+    }
+
+}
