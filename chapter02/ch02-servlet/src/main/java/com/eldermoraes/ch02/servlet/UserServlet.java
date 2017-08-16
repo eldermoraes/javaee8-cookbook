6e7a0b1d0e5aa0705d48b4c46426a1fcc3ef7279
Elder Moraes <elder.moraes@gmail.com>
2017-08-16 02:31:59 -0300

    Servlet recipe

---
+++ chapter02/ch02-servlet/src/main/java/com/eldermoraes/ch02/servlet/UserServlet.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eldermoraes.ch02.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author eldermoraes
+ */
+@WebServlet(name = "UserServlet", urlPatterns = {"/UserServlet"})
+public class UserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "UserServlet for Chapter 02";
+    }
+
+    protected void doRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet UserServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet UserServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h2>Now: " + new Date() + "</h2>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Servlet " + this.getServletName() + " has started");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet " + this.getServletName() + " has destroyed");
+    }
+
+    
+
+}
