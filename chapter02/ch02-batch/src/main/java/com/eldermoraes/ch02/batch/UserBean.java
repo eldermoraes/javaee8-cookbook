2f79a7d859d0e4144346a7f6a6706064573b706e
Elder Moraes <elder.moraes@gmail.com>
2017-08-21 19:23:49 -0300

    Batch recipe

--- chapter02/ch02-batch/src/main/java/com/eldermoraes/ch02/batch/UserBean.java
+++ chapter02/ch02-batch/src/main/java/com/eldermoraes/ch02/batch/UserBean.java
@@ -5,13 +5,13 @@ import java.util.Properties;
 import javax.batch.operations.JobOperator;
 import javax.batch.operations.JobStartException;
 import javax.batch.runtime.BatchRuntime;
-import javax.ejb.Stateless;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Named
-@Stateless
+@RequestScoped
 public class UserBean {
 
     @PersistenceContext
