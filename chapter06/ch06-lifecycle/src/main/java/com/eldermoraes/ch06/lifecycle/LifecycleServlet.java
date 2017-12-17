88fd8e9d459507ecf84f83881242ef98106faf8f
Elder Moraes <elder.moraes@gmail.com>
2017-12-17 18:36:01 -0200

    Lifecycle recipe

---
+++ chapter06/ch06-lifecycle/src/main/java/com/eldermoraes/ch06/lifecycle/LifecycleServlet.java
@@ -0,0 +1,61 @@
+package com.eldermoraes.ch06.lifecycle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author eldermoraes
+ */
+@WebServlet(name = "LifecycleServlet", urlPatterns = {"/LifecycleServlet"})
+public class LifecycleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try(PrintWriter writer = resp.getWriter()){
+            writer.write("doGet");
+            System.out.println("doGet");
+        }
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try(PrintWriter writer = resp.getWriter()){
+            writer.write("doPost");
+            System.out.println("doPost");
+        }
+    }    
+    
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try(PrintWriter writer = resp.getWriter()){
+            writer.write("doDelete");
+            System.out.println("doDelete");
+        }
+    }
+    
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try(PrintWriter writer = resp.getWriter()){
+            writer.write("doPut");
+            System.out.println("doPut");
+        }
+    }    
+    
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("init(ServletConfig config)");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+    
+}
