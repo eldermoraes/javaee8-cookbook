e627ba768b92f90e0327e2df889bff89fed85456
Elder Moraes <elder.moraes@gmail.com>
2018-02-22 06:16:35 -0300

    Async servlet recipe

---
+++ chapter10/ch10-async-servlet/src/main/java/com/eldermoraes/ch10/async/servlet/UserServlet.java
@@ -0,0 +1,41 @@
+package com.eldermoraes.ch10.async.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.inject.Inject;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author eldermoraes
+ */
+@WebServlet(name = "UserServlet", urlPatterns = {"/UserServlet"}, asyncSupported = true)
+public class UserServlet extends HttpServlet {
+
+    @Inject
+    private UserBean userBean;
+    
+    private final Jsonb jsonb = JsonbBuilder.create();
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        AsyncContext ctx = req.startAsync();
+        ctx.start(() -> {
+            try (PrintWriter writer = ctx.getResponse().getWriter()){
+                writer.write(jsonb.toJson(userBean.getUser()));
+            } catch (IOException ex) {
+                System.err.println(ex.getMessage());
+            }
+            ctx.complete();
+        });
+    }
+
+}
