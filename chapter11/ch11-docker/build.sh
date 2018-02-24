4d4d11843ce0be2aaeea9e87aa4dd31af7429fee
Elder Moraes <elder.moraes@gmail.com>
2018-02-24 08:09:29 -0300

    Docker recipe

---
+++ chapter11/ch11-docker/build.sh
@@ -0,0 +1,1 @@
+docker build -t eldermoraes/gf-javaee-cookbook .
---
+++ chapter11/ch11-docker/run.sh
@@ -0,0 +1,6 @@
+docker run -d --name gf-javaee-cookbook \
+    -p 8080:8080 \
+    -p 4848:4848 \
+    -p 8009:8009 \
+    -p 8181:8181 \
+    eldermoraes/gf-javaee-cookbook
