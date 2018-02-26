6020b4befb0b32c1ca968cc4b051522886c1f6d9
Elder Moraes <elder.moraes@gmail.com>
2018-02-26 19:12:26 -0300

    Docker and Jelastic recipes

--- chapter11/ch11-docker/run.sh
+++ chapter11/ch11-docker/run.sh
@@ -1,6 +1,8 @@
 docker run -d --name gf-javaee-cookbook \
-    -p 8080:8080 \
+    -h gf-javaee-cookbook \
+    -p 80:8080 \
     -p 4848:4848 \
+    -p 8686:8686 \
     -p 8009:8009 \
     -p 8181:8181 \
     eldermoraes/gf-javaee-cookbook
