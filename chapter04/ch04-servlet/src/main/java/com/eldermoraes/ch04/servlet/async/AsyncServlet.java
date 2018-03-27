01ca87a92ca37d925ee9bf1820270b5bf410175d
Elder Moraes <elder.moraes@gmail.com>
2018-03-27 07:56:53 -0300

    Minor fixes

--- chapter04/ch04-servlet/src/main/java/com/eldermoraes/ch04/servlet/async/AsyncServlet.java
+++ chapter04/ch04-servlet/src/main/java/com/eldermoraes/ch04/servlet/async/AsyncServlet.java
@@ -1,7 +1,6 @@
 package com.eldermoraes.ch04.servlet.async;
 
 import java.io.IOException;
-import java.util.concurrent.ThreadPoolExecutor;
 import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,32 +14,39 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(urlPatterns = "/AsyncServlet", asyncSupported = true)
 public class AsyncServlet extends HttpServlet {
+
     private static final long serialVersionUID = 1L;
- 
+
     @Override
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
+        
         long startTime = System.currentTimeMillis();
         System.out.println("AsyncServlet Begin, Name="
                 + Thread.currentThread().getName() + ", ID="
                 + Thread.currentThread().getId());
- 
+
         String time = request.getParameter("timestamp");
-        int secs = Integer.valueOf(time);
- 
+
         AsyncContext asyncCtx = request.startAsync();
-        asyncCtx.addListener(new AsyncWebListener());
-        asyncCtx.setTimeout(30000);
- 
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) request
-                .getServletContext().getAttribute("async-exec");
- 
-        executor.execute(new AsyncExecutor(asyncCtx, secs));
-        long endTime = System.currentTimeMillis();
-        System.out.println("AsyncServlet Finish, Name="
-                + Thread.currentThread().getName() + ", ID="
-                + Thread.currentThread().getId() + ", Duration="
-                + (endTime - startTime) + " milliseconds.");
+
+        asyncCtx.start(() -> {
+            try {
+                Thread.sleep(Long.valueOf(time));
+                long endTime = System.currentTimeMillis();
+                long timeElapsed = endTime - startTime;
+                System.out.println("AsyncServlet Finish, Name="
+                        + Thread.currentThread().getName() + ", ID="
+                        + Thread.currentThread().getId() + ", Duration="
+                        + timeElapsed + " milliseconds.");
+
+                asyncCtx.getResponse().getWriter().write("Async process time: " + timeElapsed + " milliseconds");
+                asyncCtx.complete();
+            } catch (InterruptedException | IOException ex) {
+                System.err.println(ex.getMessage());
+            }
+        });
+
     }
- 
+
 }
