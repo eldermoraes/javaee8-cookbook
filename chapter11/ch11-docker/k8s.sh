f2d4f9438bd2dc6201c7dc317afb7043b0af721a
Elder Moraes <elder.moraes@gmail.com>
2019-10-04 02:57:52 +0300

    Whatever...

--- chapter11/ch11-docker/k8s.sh
+++ chapter11/ch11-docker/k8s.sh
@@ -2,4 +2,6 @@ kubectl create -f app.yaml
 
 kubectl expose deployment gf-jakartaee-cookbook --type=LoadBalancer --name=gf-jakartaee-cookbook-lb
 
-kubectl delete services gf-jakartaee-cookbook-lb
+# IF YOU WANT TO DELETE THE LOAD BALANCER
+# 
+# kubectl delete services gf-jakartaee-cookbook-lb
